a469b0e1f59ac59bc117e28a2f7660af60fd1be8
ShahanMirza <34271340+ShahanMirza@users.noreply.github.com>
2021-03-23 00:32:55 +0500

    Happy pakistan day

--- python-1st.py
+++ python-1st.py
@@ -166,7 +166,97 @@
 # numlegs=94
 # solutions=solve(numheads,numlegs)
 # print (solutions )
+#string reverse
+# s=input()
+# s=s[::-1]
+# print(s)
+# import math
+# c=50
+# h=30
+# value=[]
+# items=[x for x in input().split(",")]
+# for d in items:
+#     value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# print(",".join(value))
 
-s=input()
-s=s[::-1]
-print(s)
+# input_str=input()
+# dimensions=[int(x) for x in input_str.split(',')]
+# rowNum=dimensions[0]
+# colNum=dimensions[1]
+# multilist=[[0 for col in range(colNum)]for row in range(rowNum)]
+
+# for row in range(rowNum):
+#     for col in range(colNum):
+#         multilist[row][col]=row*col
+# print(multilist)
+#sorting 
+# items=[x for x in input().split(',')]
+# items.sort()
+# print(','.join(items))
+
+#uper case
+# lines=[]
+# while True:
+#     s=input()
+#     if s:
+#         lines.append(s.upper())
+#     else:
+#         break
+# for sentence in lines:
+#     print(sentence)
+
+#sorting
+# s= input()
+# words=[word for word in s.split(" ")]
+# print(" ".join(sorted(list(set(words)))))
+
+# list=[]
+# items=[x for x in input().split(',')]
+# for p in items:
+#     intp=int(p,2)
+#     if not intp%5:
+#         list.append(p)
+# print(','.join(list))
+
+# values=[]
+# for i in range(1000,9001):
+#     s=str(i)
+#     if (int(s[0])%2==0) and (int(s[1])%2==0) and (int(s[2])%2==0) and (int(s[3])%2==0):
+#         values.append(s)
+# print(",".join(values))
+
+#calculate the number of letters and digits
+
+# consoleinput=input()
+# d={"DIGITS":0,"LETTERS":0}
+# for c in consoleinput:
+#     if c.isdigit():
+#         d['DIGITS']+=1
+#     elif c.isalpha():
+#         d['LETTERS']+=1
+#     else:
+#         break
+# print("LETTERS",d["LETTERS"])
+# print("DIGITS",d["DIGITS"])
+
+# value=input()
+# d={"UPERCARSE":0,"LOWERCASE":0}
+# for c in value:
+#     if c.isupper():
+#         d["UPERCARSE"]+=1
+#     elif c.islower():
+#         d["LOWERCASE"]+=1
+# print("UPERCARSE",d["UPERCARSE"])
+# print("LOWERCASE",d["LOWERCASE"])
+
+
+# a = input()
+# n1 = int( "%s" % a )
+# n2 = int( "%s%s" % (a,a) )
+# n3 = int( "%s%s%s" % (a,a,a) )
+# n4 = int( "%s%s%s%s" % (a,a,a,a) )
+# print (n1+n2+n3+n4)
+
+# value=input()
+# numbers=[x for x in value.split(',') if int(x)%2!=0]
+# print(",".join(numbers))
